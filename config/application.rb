a4113fa60c47665b93b0d5563ca1e24b71affd95
Jeremy Z <akajeremy@comcast.net>
2015-06-08 14:27:41 -0400

    iframes and emailing

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,17 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+}
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Codermanual
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
