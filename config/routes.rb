e9aeaf7271087d64c87303b3749785ca06258957
Jeremy Z <akajeremy@comcast.net>
2015-06-11 09:29:49 -0400

    updated routes with devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   resources :contacts
   get 'pages/home'
   get '/about' => 'pages#about'
